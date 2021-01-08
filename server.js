096f3f66b86da1d90975bcc48d71633f25219f1d
guokenlin1 <59401241+guokenlin1@users.noreply.github.com>
2021-01-08 17:24:56 -0500

    in progress

--- server.js
+++ server.js
@@ -6,10 +6,21 @@ const multer = require("multer");
 const body = require("body-parser");
 const randtoken = require("rand-token");
 const fs = require("fs");
+const mongoose = require('mongoose');
 
-app.use(express.static("public"));
+
+app.use(express.static(__dirname + "/public"));
+// app.use(express.static(__dirname + "/HTML"));
 app.use(body.urlencoded({ extended: true }));
-
+//connect to database "tokenDB"
+mongoose.connect('mongodb://localhost:27017/userDB', { useNewUrlParser: true, useUnifiedTopology: true });
+const userSchema = new mongoose.Schema({
+    email: String,
+    password: String,
+    userToken: String
+})
+//create a new collection "User", all records contains username and password will be saved in the db
+const User = mongoose.model('USER', userSchema);
 let token = "";
 
 //used to specify file destination on local system after uploading
@@ -18,6 +29,7 @@ let storage = multer.diskStorage({
         // Generate a 16 character alpha-numeric token
         token = randtoken.generate(16);
         console.log(token);
+
         if (!fs.existsSync(__dirname + '/uploads/' + token)) {
             fs.mkdirSync(__dirname + '/uploads/' + token);
         }
@@ -36,9 +48,9 @@ let upload = multer({
     // myFile is the name of file attribute  
 }).single("myFile");
 
-app.get("/upload", function (req, res) {
-    res.sendFile(__dirname + "/main.html"); //change html to static content, like sytles.css
-})
+// app.get("/upload", function (req, res) {
+//     res.sendFile(__dirname + "/public/upload/main.html"); //change html to static content, like sytles.css
+// })
 
 app.post("/uploadFileAndGetFileUrl", (req, res) => {
 
@@ -66,22 +78,17 @@ app.get('/downloadFileWithRandomUrl/:token', (req, res) => {
     fs.readdir(folder, (err, files) => {
         //fileName is the first file(only one) under the folder
         let fileName = files[0];
-      
+
         let dir = path.join(__dirname, 'uploads/' + token + '/' + fileName);
         console.log(dir);
-        // let response = document.getElementById("response");
-        // if (res.statusCode === 200){
-        //    response = "File url: " + dir;
-        // }else{
-        //     response = "Error! Please try again."
-        // }
+
         res.download(dir, fileName, function (error) {
             console.log("Error: ", error);
 
         });
     });
-    
-    
+
+
 
     console.log("downloaded");
     //res.sendFile();
@@ -97,11 +104,74 @@ app.get('/downloadFileWithRandomUrl/:token', (req, res) => {
 app.post('/', function (req, res) {
     let email = req.body.email;
     let password = req.body.password;
+   
+    User.findOne({ email: email }, function (err, user) {
+        if (err) {
+            console.log("error");
+        }
+        else {
+            if (user === null) {
+                console.log("email doesn't exsit!");
+                res.redirect('/');
 
-    console.log(email, password);
-    res.redirect('/upload');
+            } else {
+                if (user.password === password) {
+                    let userToken = randtoken.generate(20);
+                    console.log("success");
+                    User.updateOne({ email: email }, { userToken: userToken }, function (err) {
+                        if (err) {
+                            console.log(err);
+                        }
+                    })
+                    res.redirect('/upload');
+                }
+                else {
+                    console.log("Wrong password");
+                    res.redirect('/');
+
+                }
+            }
+
+
+        }
+
+    })
+   
+
 })
 
+// app.get("/signup", function(req, res){
+//     res.sendFile(__dirname + "/signup.html");
+// })
+
+app.post("/signup", function(req, res){
+    let email = req.body.email;
+    let password = req.body.password;
+    User.findOne({email:email}, function(error, record){
+        if (error){
+            console.log(error);
+        }
+        else{
+            //if the record doesn't exist, then add it to the database
+            if (record === null){
+                User.insertMany([{email:email, password:password}], function(error, record){
+                    if (error){
+                        console.log(error);
+                    }
+                    else{
+                        console.log(record);
+                    }
+                })
+                res.redirect("/");
+            }
+            else{
+                console.log("Email exists. Please try again");
+                res.redirect("/signup");
+            }
+        }
+    })
+   
+})
 // Take any port number of your choice which 
 // is not taken by any other process 
 app.listen(3000, function (error) {
