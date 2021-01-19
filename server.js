a47092932faf21e9c03f21db87bda8f6f457fe46
guokenlin1 <59401241+guokenlin1@users.noreply.github.com>
2021-01-19 15:04:00 -0500

    finish authentication

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ const body = require("body-parser");
 const randtoken = require("rand-token");
 const fs = require("fs");
 const mongoose = require('mongoose');
+const asyncHandler = require('express-async-handler')
 const { exec } = require("child_process");
 
 app.set('view engine', 'ejs');
@@ -38,6 +39,8 @@ const fileCollection = mongoose.model('FILECOLLECTION', fileSchema);
 
 let token = "";
 let currentEmail = ""; // record the current user 
+let currentUserToken = ""; // used for authentication
+
 //used to specify file destination on local system after uploading
 let storage = multer.diskStorage({
     destination: function (req, file, cb) {
@@ -55,13 +58,30 @@ let storage = multer.diskStorage({
 
         cb(null, file.originalname);
 
-    }
+    },
+
 })
+
+const fileFilter = (req, file, cb) => {
+
+    let found = fileCollection.findOne({ filename: file.filename, email: currentEmail }).exec();
+    found.then((record) => {
+        if (!record) {
+            cb(null, true);
+        }
+        else {
+            return cb(null, false, new Error("duplicate file name"));
+        }
+    })
+}
 //upload can process one file each time
 let upload = multer({
-    storage: storage //this specify file name and destination after uploading
+    storage: storage, //this specify file name and destination after uploading
+
+    //check for duplicate filename before uploading
+    fileFilter: fileFilter
     // myFile is the name of file attribute  
-}).single("myFile");
+});
 
 app.get("/upload", function (req, res) {
 
@@ -69,83 +89,135 @@ app.get("/upload", function (req, res) {
 })
 
 app.get("/home", function (req, res) {
-    
+
     fileCollection.find({ email: currentEmail }, function (err, files) {
         if (err) {
             console.log(err);
         } else {
-      
-            res.render("home", {files: files});
+
+            res.render("home", { files: files });
         }
 
     })
-   
-    
+
+
 })
 
-app.post("/uploadFileAndGetFileUrl", (req, res) => {
+app.post("/uploadFileAndGetFileUrl", asyncHandler(async (req, res) => {
 
-    upload(req, res, function (err) {
+    //upload the file
+    let uploadFile = upload.single("myFile");
 
-        if (err) {
-            console.log("error");
-            res.send(err)
+    //check if the user log in
+    if (currentEmail === "") {
+        res.send("Please log in first");
+    }
+
+    let authenticate = false;
+    //authenticate the userToken before uploading
+    const user = await User.findOne({ email: currentEmail });
+    async function authentication() {
+        if (user.userToken === currentUserToken) {
+            authenticate = true;
         }
-        else {
-            console.log(req.file.filename);
+    }
 
-            fileCollection.insertMany({ email: currentEmail, fileName: req.file.filename,
-                fileLocation: req.file.destination, token: token },
-                function (err, record) {
-                    if (err) {
-                        console.log(err);
+    //if authenticated, start uploading the file
+    async function second() {
+        if (authenticate) {
+            uploadFile(req, res, function (err) {
+
+                console.log(req.file);
+                if (err) {
+                    console.log("error");
+                    res.send(err)
+                }
+                else {
+                    if (typeof req.file === 'undefined') {
+                        console.log("duplicate files");
+                        //avoid duplicate filename error
                     }
                     else {
-                        console.log(record);
+                        fileCollection.insertMany({
+                            email: currentEmail, fileName: req.file.filename,
+                            fileLocation: req.file.destination, token: token
+                        },
+                            function (err, record) {
+                                if (err) {
+                                    console.log(err);
+                                }
+                                else {
+                                    console.log(record);
+                                }
+                            })
+                        // SUCCESS, file successfully uploaded 
                     }
-                })
-            // SUCCESS, file successfully uploaded 
-            res.render("upload");
+
+
+                }
+            })
         }
-    })
-    //generate a random url
-    //then call upload, make connection between the url and the submitted file
-})
-
-app.get('/downloadFileWithRandomUrl/:token', (req, res) => {
-
-    // Retrieve the tag from our URL path
-    let token = req.params.token;
-    //use fs module
-    const folder = __dirname + '/uploads/' + token;
-    fs.readdir(folder, (err, files) => {
-        //fileName is the first file(only one) under the folder
-        let fileName = files[0];
-
-        let dir = path.join(__dirname, 'uploads/' + token + '/' + fileName);
-        console.log(dir);
-
-        res.download(dir, fileName, function (error) {
-            console.log("Error: ", error);
-
-        });
-    });
+        else {
+            console.log("userTokens don't match");
+        }
 
 
+    }
 
-    console.log("downloaded");
-    //res.sendFile();
-})
+    await authentication();
+    await second();
+
+
+    res.render("upload");
+}))
+
+app.get('/downloadFileWithRandomUrl/:token', asyncHandler(async (req, res) => {
+
+    let authenticate = false;
+    //authenticate the userToken before uploading
+    const user = await User.findOne({ email: currentEmail });
+    async function authentication() {
+        if (user.userToken === currentUserToken) {
+            authenticate = true;
+        }
+    }
+    async function second() {
+        if (authenticate) {
+            // Retrieve the tag from our URL path
+            let token = req.params.token;
+            //use fs module
+            const folder = __dirname + '/uploads/' + token;
+            fs.readdir(folder, (err, files) => {
+                //fileName is the first file(only one) under the folder
+                let fileName = files[0];
+
+                let dir = path.join(__dirname, 'uploads/' + token + '/' + fileName);
+                console.log(dir);
+
+                res.download(dir, fileName, function (error) {
+                    console.log("Error: ", error);
+
+                });
+            });
+        }
+    }
+    await authentication();
+    await second();
+
+}))
 
 //sign in page
 app.get('/', function (req, res) {
     res.render("signin");
 })
 
-//after successfully signing in, go to main page
-app.post('/', function (req, res) {
+/**
+ * after successfully signing in, go to main page
+ */
+app.post('/login', function (req, res) {
     let email = req.body.email;
     let password = req.body.password;
+
 
     User.findOne({ email: email }, function (err, user) {
         if (err) {
@@ -154,18 +226,24 @@ app.post('/', function (req, res) {
         else {
             if (user === null) {
                 console.log("email doesn't exsit!");
-                res.redirect('/');
+                res.render('signin', { user: user });
 
             } else {
                 if (user.password === password) {
                     currentEmail = email; // update current email
                     let userToken = randtoken.generate(20);
                     console.log("success");
+
                     User.updateOne({ email: email }, { userToken: userToken }, function (err) {
                         if (err) {
                             console.log(err);
                         }
+                        else {
+                            currentUserToken = userToken;
+                            console.log("userToken: " + currentUserToken);
+                        }
                     })
+
                     console.log(currentEmail);
                     res.redirect('/upload');
                 }
@@ -216,6 +294,36 @@ app.post("/signup", function (req, res) {
     })
 
 })
+
+/**
+ * delete a specified file
+ */
+app.get("/delete/:token", asyncHandler(async function (req, res) {
+    let authenticate = false;
+    //authenticate the userToken before uploading
+    const user = await User.findOne({ email: currentEmail });
+    async function authentication() {
+        if (user.userToken === currentUserToken) {
+            authenticate = true;
+        }
+    }
+    async function second() {
+        let token = req.params.token;
+        fileCollection.deleteOne({ token: token }, function (err) {
+            if (err) {
+                console.log(err);
+            } else {
+                res.redirect("/home");
+            }
+        });
+    }
+
+    await authentication();
+    await second();
+
+
+}))
+
 // Take any port number of your choice which 
 // is not taken by any other process 
 app.listen(3000, function (error) {
